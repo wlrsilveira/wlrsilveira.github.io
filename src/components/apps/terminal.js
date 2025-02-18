c291a2ba3c0b1b7ac79a6d35c2af072cdc9e91bf
Wagner Silveira <wagner.resta@outlook.com>
2025-02-18 13:21:10 -0300

    Update terminal.js

--- src/components/apps/terminal.js
+++ src/components/apps/terminal.js
@@ -14,7 +14,7 @@ export class Terminal extends Component {
         this.child_directories = {
             root: ["books", "projects", "personal-documents", "skills", "languages", "PDPU", "interests"],
             PDPU: ["Sem-6"],
-            books: ["Eric-Jorgenson_The-Almanack-of-Naval-Ravikant.pdf", "Elon Musk: How the Billionaire CEO of SpaceX.pdf", "The $100 Startup_CHRIS_GUILLEBEAU.pdf", "The_Magic_of_Thinking_Big.pdf"],
+            books: ["Wagner-Resume.pdf", "Elon Musk: How the Billionaire CEO of SpaceX.pdf", "The $100 Startup_CHRIS_GUILLEBEAU.pdf", "The_Magic_of_Thinking_Big.pdf"],
             skills: ["Front-end development", "React.js", "jQuery", "Flutter", "Express.js", "SQL", "Firebase"],
             projects: ["vivek9patel-personal-portfolio", "synonyms-list-react", "economist.com-unlocked", "Improve-Codeforces", "flutter-banking-app", "Meditech-Healthcare", "CPU-Scheduling-APP-React-Native"],
             interests: ["Software Engineering", "Deep Learning", "Computer Vision"],
